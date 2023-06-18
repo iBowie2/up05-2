f3a67248b34c19783cd5974f70681261e20ef6cf
iBowie2 <133043118+iBowie2@users.noreply.github.com>
2023-06-18 13:47:16 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var numberbox = document.getElementById('numberbox');
+var total = document.getElementById('total');
+var singleBuyCost = document.getElementById('singleBuyCost');
+var regularBuyCost = document.getElementById('regularBuyCost');
+
+var costPerKg = 250;
+var discountRegular = -100;
+
+function addAmount(amount)
+{
+    numberbox.value = Math.max(1, Number(numberbox.value) + amount);
+    recalculate();
+    return false;
+}
+
+function recalculate()
+{
+    var selectedVolume = document.querySelector('input[name="volume"]:checked').value;
+    var selectedSubType = document.querySelector('input[name="subtype"]:checked').value;
+    var count = Number(numberbox.value);
+
+    singleBuyCost.innerHTML = (costPerKg * selectedVolume * count) + " ₽";
+    regularBuyCost.innerHTML = (costPerKg * selectedVolume * count + discountRegular) + " ₽";
+
+    if (selectedSubType == 'once')
+    {
+        total.innerHTML = singleBuyCost.innerHTML;
+    }
+    else
+    {
+        total.innerHTML = regularBuyCost.innerHTML;
+    }
+
+    return false;
+}
+
+recalculate();
